775c51870ff636ccb359f67abc03d779ba5a57f7
edoedac0 <edinvelic2011@gmail.com>
2026-03-21 12:48:08 +0100

    chore: bump app version to 0.1.2

--- js/ui/screens/settings/settingsScreen.js
+++ js/ui/screens/settings/settingsScreen.js
@@ -38,7 +38,7 @@ import {
 const ROTATED_DPAD_KEY = "rotatedDpadMapping";
 const STRICT_DPAD_GRID_KEY = "strictDpadGridNavigation";
 const SETTINGS_UI_STATE_KEY = "settingsScreenUiState";
-const SETTINGS_VERSION_LABEL = "0.1.3-web";
+const SETTINGS_VERSION_LABEL = "0.1.2";
 const PRIVACY_URL = "https://tapframe.github.io/NuvioStreaming/#privacy-policy";
 const SUPPORTERS_URL = "https://github.com/Tapframe/NuvioStreaming";
 
